f6c6279a2a1e8d7442dab1ad6edd838b73abbc65
Reza Soltani <soltanireza65@.github.com>
2020-08-16 20:22:22 +0430

    Login Fixed

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,7 +6,7 @@ from accounts.models import Profile
 
 class LoginForm(forms.Form):
     username = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control input_user border-input', 'placeholder': 'Username'}))
+        widget=forms.TextInput(attrs={'class': 'form-control input_user border-input', 'placeholder': 'Email'}))
     password = forms.CharField(
         widget=forms.PasswordInput(attrs={'class': 'form-control input_pass', 'placeholder': 'Password'}))
 
